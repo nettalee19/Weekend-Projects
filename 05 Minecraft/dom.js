ad0e2dcac6b1159857760cea0509548596413808
Netta Avidan <nettavidan19@gmail.com>
2021-02-25 17:47:44 +0200

    5

--- 05 Minecraft/dom.js
+++ 05 Minecraft/dom.js
@@ -1,20 +1,41 @@
-let world = document.querySelector("#world")
 let nav = document.querySelector("#nav-bar")
 
+let world = document.querySelector("#world")
 let worldWidth = parseInt(window.getComputedStyle(world).width)
 let worldHeight = parseInt(window.getComputedStyle(world).height)
 
+//height:fit-content; container div
 
-
-const createSky = () =>{
-  for(let i =0; i< worldHeight/20 ;i++){
-    for(let j=0; j< worldWidth/20; j++){
+let skyMatrix = [[]]
+//const createSky = () =>{
+  for(let i =0; i< worldHeight/27 ;i++){
+    for(let j=0; j< worldWidth/27; j++){
+      
       let sky = document.createElement('div')
       sky.classList.add('sky')
       sky.setAttribute('row', i)
       sky.setAttribute('column', j)
+      skyMatrix.push([i,j])
       world.appendChild(sky)
-      console.log(sky)
+      //console.log(sky)
+      
     }
+  }
+//}
+
+//todo dfdf
+let groundMatrix = [[]]
+
+for(let i =16; i< worldHeight/27 ;i++){
+  for(let j=0; j< worldWidth/27; j++){
+    
+    let ground = document.createElement('div')
+    ground.classList.add('ground')
+    ground.setAttribute('row', i)
+    ground.setAttribute('column', j)
+    groundMatrix.push([i,j])
+    world.appendChild(ground)
+    //console.log(sky)
+    
   }
 }
