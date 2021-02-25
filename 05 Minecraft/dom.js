8cfe013bb0ea574f6910c67d9c458a44495d5969
Netta Avidan <nettavidan19@gmail.com>
2021-02-25 15:16:34 +0200

    05

--- 05 Minecraft/dom.js
+++ 05 Minecraft/dom.js
@@ -1,5 +1,20 @@
-let world = document.querySelector(".world")
-let nav = document.querySelector(".nav-bar")
+let world = document.querySelector("#world")
+let nav = document.querySelector("#nav-bar")
+
+let worldWidth = parseInt(window.getComputedStyle(world).width)
+let worldHeight = parseInt(window.getComputedStyle(world).height)
 
 
-world.style.backroundcolor = "lightblue"
+
+const createSky = () =>{
+  for(let i =0; i< worldHeight/20 ;i++){
+    for(let j=0; j< worldWidth/20; j++){
+      let sky = document.createElement('div')
+      sky.classList.add('sky')
+      sky.setAttribute('row', i)
+      sky.setAttribute('column', j)
+      world.appendChild(sky)
+      console.log(sky)
+    }
+  }
+}
